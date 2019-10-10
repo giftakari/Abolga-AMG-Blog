0f82edd4d6ed09894978e576a9bd97cab48e7db5
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-10-10 13:55:27 +1100

    complete static pages setup

--- index.js
+++ index.js
@@ -8,4 +8,16 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/index.html"));
 });
 
+app.get("/about", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+});
+
+app.get("/post", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/post.html"));
+});
+
 app.listen(5000, () => console.log("App is running at port 5000"));
