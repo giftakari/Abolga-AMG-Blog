b33b173efc7e66d324e6467d3356b1f6bce677c0
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-10-10 16:59:11 +1100

    database setup

--- index.js
+++ index.js
@@ -4,15 +4,36 @@ const path = require("path");
 const pug = require("pug");
 const { config, engine } = require("express-edge");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+//Connect Mongoose to our application
+mongoose
+  .connect("mongodb://localhost:27017/abolga-amg-blog", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => "DB connected!!!!!!!!!")
+  .catch(err => console.log("Something went wrong", err));
 
 // Our Middleware
 app.use(express.static("public"));
 app.use(engine);
 app.set("views", `${__dirname}/views`);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Our routes
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("create");
+});
+
+app.post("/post/create", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.get("/about", (req, res) => {
