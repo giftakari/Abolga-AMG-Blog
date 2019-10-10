9023972c88d424db821009ca7d4e59df5ebb261e
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-10-10 13:23:16 +1100

    Added pages

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+});
+
+app.listen(5000, () => console.log("App is running at port 5000"));
