dfb215c8cd3db4e9036899a89e077102cfe316fc
Godsgift ThankGod Akari <giftakari@yahoo.com>
2019-10-10 14:58:16 +1100

    template starter

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const pug = require("pug");
+const { config, engine } = require("express-edge");
+const mongoose = require("mongoose");
 
+// Our Middleware
 app.use(express.static("public"));
+app.use(engine);
+app.set("views", `${__dirname}/views`);
 
+//Our routes
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
@@ -20,4 +27,5 @@ app.get("/post", (req, res) => {
   res.sendFile(path.resolve(__dirname, "pages/post.html"));
 });
 
+// Listen for incoming request
 app.listen(5000, () => console.log("App is running at port 5000"));
